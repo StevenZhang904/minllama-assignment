cc726417f6ba5a11e552aceccac2d67866f2a0b2
StevenZhang904 <53510050+StevenZhang904@users.noreply.github.com>
2024-02-12 15:52:43 -0500

    optimizer finished

--- optimizer.py
+++ optimizer.py
@@ -38,7 +38,7 @@ class AdamW(Optimizer):
                 if grad.is_sparse:
                     raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
 
-                raise NotImplementedError()
+                # raise NotImplementedError()
 
                 # State should be stored in this dictionary
                 state = self.state[p]
@@ -47,14 +47,35 @@ class AdamW(Optimizer):
                 alpha = group["lr"]
 
                 # Update first and second moments of the gradients
+                if len(state) == 0:
+                    state['timestep'] = 0
+                    state['m'] = torch.zeros_like(p.data)
+                    state['v'] = torch.zeros_like(p.data)
+                
+                m = state['m']
+                v = state['v']
+                t = state['timestep']
+                t+=1
+                beta1, beta2 = group['betas']
+                
+                m = m * beta1 + (1-beta1) * grad
+                v = v * beta2 + (1-beta2) * (grad**2)
 
+                
                 # Bias correction
                 # Please note that we are using the "efficient version" given in
                 # https://arxiv.org/abs/1412.6980
-
+                alpha_updated = alpha * ((1-beta2**t)**0.5) / (1-beta1**t)
+            
                 # Update parameters
-
+                grad = alpha_updated * m / (v**0.5 + group['eps'])
                 # Add weight decay after the main gradient-based updates.
                 # Please note that the learning rate should be incorporated into this update.
+
+                state['lr'] = alpha_updated
+                state['timestep'] = t
+                state['m'] = m
+                state['v'] = v
+                p.data -= grad
 
         return loss
